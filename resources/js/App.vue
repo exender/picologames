3681a1da6f9c1c4245419b9ff1c60cf13ade4e84
exender <baptistebonnin4@gmail.com>
2024-01-25 15:34:46 +0100

    feat:socket_are_working

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -10,7 +10,7 @@
 export default {
 	mounted() {
 		window.Echo.channel('channel')
-			.listen('Hello', (e) => {
+			.listen('Test', (e) => {
 				console.log(e)
 			})
 	},
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,7 +36,7 @@ window.Pusher = require('pusher-js')
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: process.env.DB_HOST,
+    wsHost: window.location.hostname,
     wsPort: 6001,
     forceTLS: false,
     disableStats: true,
